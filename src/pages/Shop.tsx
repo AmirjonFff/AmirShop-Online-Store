10d252bd58a2faab68e32bff0a287008ad1ff01e
Aminjon Ffff <amirjonif@gmail.com>
2024-04-26 15:08:24 +0500

    search text

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,10 +1,13 @@
-import { Container } from "@mantine/core"
+import { Box, Container, Title } from "@mantine/core"
 import HomeDevices from "../components/HomeDevices"
 import { Toaster } from "react-hot-toast"
 
 function Shop() {
   return (
     <Container size={1200}>
+      <Box>
+        <Title>Результаты поиска: "{}"</Title>
+      </Box>
       <HomeDevices />
       <Toaster
         position="top-right"
