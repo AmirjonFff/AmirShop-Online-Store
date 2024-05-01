655121abdcd409021e31a4bdc0be4fbea1a58a8f
Aminjon Ffff <amirjonif@gmail.com>
2024-05-01 15:04:03 +0500

    del btn

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
-import { AppShell, Box, Button, Container, Group, UnstyledButton, rem } from '@mantine/core';
+import { AppShell, Box, Container, Group, UnstyledButton, rem } from '@mantine/core';
 import { useHeadroom } from '@mantine/hooks';
-import { IconLogin2, IconShoppingCart } from '@tabler/icons-react';
+import { IconShoppingCart } from '@tabler/icons-react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
@@ -50,7 +50,7 @@ export function Headroom({ children }: IHeadroom) {
   ]
 
   return (
-    <AppShell header={{ height: 85, collapsed: !pinned, offset: false }} padding="md">
+    <AppShell header={{ height: 77, collapsed: !pinned, offset: false }} padding="md">
       <AppShell.Header>
         <Container className='w-full' size="1200px">
           <Group className="flex justify-between items-center border-b border-[#3a539d84] py-6">
@@ -60,7 +60,7 @@ export function Headroom({ children }: IHeadroom) {
             <Group className="flex gap-11 text-[#3D3D3D] items-center text-[16px] translate-y-1">
               {navData.map(nav =>
                 <UnstyledButton key={nav.name} className={`cursor-pointer ${isActive === nav.path && 'font-bold'}`}><Link to={nav.path}>{nav.name}</Link>
-                  {isActive === nav.path && <Box className="h-[3px] bg-[#3a539d] w-full translate-y-6"></Box>}</UnstyledButton>
+                  {isActive === nav.path && <Box className="h-[3px] bg-[#3a539d] w-full translate-y-5"></Box>}</UnstyledButton>
               )}
             </Group>
             <Group className="flex gap-7 items-center">
@@ -71,9 +71,9 @@ export function Headroom({ children }: IHeadroom) {
                   {cartTotalQuantity}
                 </Box>}
               </Box>
-              <Button leftSection={<IconLogin2 size={24} />} color="#3a539d" variant="filled">
+              {/* <Button leftSection={<IconLogin2 size={24} />} color="#3a539d" variant="filled">
                 Авторизоваться
-              </Button>
+              </Button> */}
             </Group>
           </Group>
         </Container>
